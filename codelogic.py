ac4a883ea996676cf58a6a7788fb6dfa1a9a6c17
MD. Zihad Islam <159674523+md-zihad-coder@users.noreply.github.com>
2026-06-27 22:14:20 +0600

    Add files via upload

---
+++ codelogic.py
@@ -0,0 +1,15 @@
+#Variable :
+#       A variable is a name used to store data in a program
+
+name = "Zihad"
+print("My name is ",name)
+
+age = 20
+print("i am ",age,"years old")
+
+n=25
+print(f"i am {n} Years old")
+
+
+
+
