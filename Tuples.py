a4d9fff27af47d3c613128e2f4ceccef3d2abcf6
MD. Zihad Islam <159674523+md-zihad-coder@users.noreply.github.com>
2026-06-29 23:19:25 +0600

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+#A tuple is a fixed list of items ordered in a specific sequence.
+tp = (
+    "zihad",
+    "Zara",
+    "Farhan"
+)
+print(tp[0])
+
+students = (20, 21, 22, 23, 25)
+print(students[2])
