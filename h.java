f2669f6ade736588978b9d9675e5122b49adb206
MD. Zihad Islam <159674523+md-zihad-coder@users.noreply.github.com>
2026-07-03 23:22:08 +0600

    Add files via upload

---
+++ h.java
@@ -0,0 +1,17 @@
+
+class ht{
+ String name ;
+ int age;
+ int roll;
+}
+
+class h{
+    public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    ht obj = new ht();
+    obj.name = "zihad";
+    obj.age = 20;
+    System.out.println("Name: " + obj.name);
+    System.out.println("Age: " + obj.age);
+}
+}
