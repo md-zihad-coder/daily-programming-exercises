aeec7870fb238b01eef10612cd0c1e55e16d8114
MD. Zihad Islam <159674523+md-zihad-coder@users.noreply.github.com>
2026-07-04 23:18:41 +0600

    Add files via upload

---
+++ class_object.java
@@ -0,0 +1,22 @@
+class h
+{
+    void cat() {
+    System.out.println("japans cat");
+ }
+ void dog(){
+     System.out.println("Brazil dog");
+ }
+
+ private void bird(){
+     System.out.println("USA parrot");
+ }
+
+public static void main(String[] args) {
+
+
+    h s= new h();
+    s.cat();
+    s.dog();
+    s.bird();
+}
+}
