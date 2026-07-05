7c674fab4b9c8f9b82f2c291a0352f79bb164a72
MD. Zihad Islam <159674523+md-zihad-coder@users.noreply.github.com>
2026-07-05 21:43:33 +0600

    #construcator

---
+++ Constructors.java
@@ -0,0 +1,22 @@
+class con {
+             //Constructor
+    con(String c , int d){
+
+        System.out.println("Name is : "+c);
+        System.out.println("Age is : "+d);
+    }
+
+
+    public static void main(String[] args) {
+
+        con obj = new con("MD. Z",20);
+        con obj1 = new con("MST. M", 16);
+        con obj2= new con("MD. x", 25);
+        con obj3= new con("MST. R", 34);
+        con obj4= new con("MD. E ", 43);
+
+
+
+    }
+
+}
